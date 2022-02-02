72dfe6fe0c823d097e97e975fb1ad3b6ae5b65f6
lp <laxmi.p@simformsolutions.com>
2022-02-02 15:30:36 +0530

    callback added in faculty and student table

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -16,4 +16,50 @@ class Faculty < ApplicationRecord
     end
   end
 
+  #callback
+  before_validation :normalize_name
+  after_validation  :DOB_validated
+  after_create  :show_create
+  after_update  :show_update
+  after_destroy :show_destroy
+  after_save  :show_save
+  after_commit :show_Email_validated
+  private
+
+  after_initialize do |faculty|
+    puts "You have initialized faculty  object!"
+  end
+
+  after_find do |faculty|
+    puts "You have found faculty  object!"
+  end
+
+  def normalize_name
+    self.first_name = first_name.downcase.titleize
+    self.last_name = last_name.downcase.titleize
+  end
+
+  def show_create 
+    puts "record is successfully created."
+  end
+
+  def show_destroy
+    puts "record is successfully deleted."
+  end
+
+  def show_update
+    puts "record is successfully updated."
+  end
+
+  def show_save
+    puts "your changes has been saved."
+  end
+
+  def DOB_validated
+    puts "Your date of birth is validated."
+  end
+
+  def show_Email_validated
+    puts "Email validated."
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -12,4 +12,56 @@ class Student < ApplicationRecord
       errors.add(:department, "can't be #{department} ")
     end
   end
+
+  #callback
+  before_validation :normalize_name
+  before_validation  :DOB_validated
+  after_create  :show_create
+  after_update  :show_update
+  # after_destroy :show_destroy
+  after_save  :show_save
+  after_destroy :call_after_destroy_commit, if: after_destroy_commit
+  private
+
+  after_initialize do |student|
+    puts "You have initialized student object!"
+  end
+
+  after_find do |student|
+    puts "You have found student object!"
+  end
+
+  def normalize_name
+    self.first_name = first_name.downcase.titleize
+    self.last_name = last_name.downcase.titleize
+  end
+
+  def show_create 
+    puts "record is successfully created."
+  end
+
+  def show_destroy
+    puts "record is successfully deleted."
+  end
+
+  def show_update
+    puts "record is successfully updated."
+  end
+
+  def show_save
+    puts "your changes has been saved."
+  end
+
+  def DOB_validated
+    puts "Your date of birth cannot be in future."
+  end
+  
+  def destroy_student
+    puts "student destroyed successfully"
+  end
+ 
+  def call_after_destroy_commit
+    puts "this is the confirmation that your record is deleted."
+  end
+ 
 end
