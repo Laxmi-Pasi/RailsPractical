5fb8de81f062bb5e976585e71cb928e083c0950e
lp <laxmi.p@simformsolutions.com>
2022-02-10 16:52:42 +0530

    completed till task 13 in advance query interface

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -34,7 +34,7 @@ class OrdersController < ApplicationController
     @order = Order.find(params[:id])
     respond_to do |format|
       if @order.update(order_params)
-        format.html { redirect_to product_url(@order), notice: "Order was successfully updated." }
+        format.html { redirect_to order_url(@order), notice: "Order was successfully updated." }
         format.json { render :show, status: :ok, location: @order }
       else
         format.html { render :edit, status: :unprocessable_entity }
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 Rails.application.routes.draw do
   resources :customers
-  resources :orders
+  #resources :orders
   #resources :products
   resources :products, only: [:index, :show, :delete, :new, :create, :edit, :update] do
     collection do
       get :display_all_products
       get :display_products_which_is_active
+      get :most_sales_product_by_quantity
+      get :most_sales_product_by_price
     end
   end
-  #get "products/display_all_product", to:"products#display_all_product"
+  resources :orders, only: [:index, :show, :delete, :new, :create, :edit, :update] do
+    collection do
+      get :most_booked_order_of_product
+      get :min_cancelled_order_of_product
+    end
+  end
   root "articles#index"
   get "/articles", to: "articles#index"
 end
