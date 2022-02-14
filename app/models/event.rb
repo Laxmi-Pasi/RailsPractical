9521e7f912c3f3f0b025545b4c78e3be7af6ddf0
lp <laxmi.p@simformsolutions.com>
2022-02-14 13:20:21 +0530

    Created Event CRUD app with name, description, event_date

---
+++ app/models/event.rb
@@ -0,0 +1,5 @@
+class Event < ApplicationRecord
+  validates :name, :description, :event_date, presence: true
+  validates :description, length: {minimum:6, maximum: 50}
+  validates :event_date, comparison: {greater_than_or_equal_to: Date.today}
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :products
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  resources :events
   root "articles#index"
   get "/articles", to: "articles#index"
 end
