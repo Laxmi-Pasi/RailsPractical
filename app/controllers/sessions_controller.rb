42bc7b4a4b60e5ad300f87f512c9345c003b0fa7
Laxmi-Pasi <97436344+Laxmi-Pasi@users.noreply.github.com>
2022-02-17 16:52:22 +0530

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,9 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       session[:user_id]=user.id
       flash[:notice]="Logged in successfully"
-      puts "----------------logged in--------------------"
       redirect_to user
     else
       flash[:notice]="invalid email or password."
-      puts "--------------------invalid---------------------"
       redirect_to login_path
     end
   end
